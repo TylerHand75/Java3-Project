0da410352ccabc12c436c1476ec333c53ab52a83
Tyler <Tylerhand75@gmail.com>
2023-02-08 14:14:48 -0600

    Class commits

--- src/main/java/Twilio/SMSOut.java
+++ src/main/java/Twilio/SMSOut.java
@@ -1,5 +1,7 @@
 package Twilio;
-
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
 
@@ -11,14 +13,28 @@ import java.util.Map;
 
 @WebServlet(name = "SMSOut", value = "/send-sms")
 public class SMSOut extends HttpServlet {
+    private String TWILIO_PHONE;
+    private String TWILIO_SID;
+    private String TWILIO_KEY;
+    @Override
+    public void init() throws ServletException {
+        Dotenv dotenv = Dotenv.configure().load();
+        TWILIO_PHONE = dotenv.get("TWILIO_PHONE");
+        TWILIO_SID = dotenv.get("TWILIO_SID");
+        TWILIO_KEY = dotenv.get("TWILIO_KEY");
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.out.println(dotenv.get("TWILIO_PHONE"));
-        System.out.println(dotenv.get("TWILIO_SID"));
-        System.out.println(dotenv.get("TWILIO_KEY"));
+        Twilio.init(TWILIO_SID, TWILIO_KEY);
 
+        Message message = Message.creator(new PhoneNumber("+13194321104"),
+                new PhoneNumber(TWILIO_PHONE),
+                "Yooooooooooooooooooo Bro ").create();
+
+        System.out.println(message.getSid());
     }
+
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
